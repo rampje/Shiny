75d62d1b71c70437efb9d06a3b32eeea6358f4ea
Warner <rampy@vt.edu>
2017-02-19 22:31:17 -0500

    repurposed app to stream PredictIt market data

--- server.R
+++ server.R
@@ -1,31 +1,53 @@
 library(shiny)
 library(forecast)
 library(ggplot2)
+library(httr)
+library(httpuv)
+library(jsonlite)
+library(magrittr)
+library(DT)
 
-series <- read.csv("DGS10.csv", stringsAsFactors = FALSE)
+shinyServer(function(input, output, session) {
+  
+  # # function to get new data from api 
+  GetData <- function(ticker){
+    
+    marketLink <- 
+      paste0("https://www.predictit.org/api/marketdata/ticker/",ticker)
+    
+    marketContent <- httr::GET(marketLink)
+    marketData <- httr::content(marketContent)
+    marketData <- jsonlite::fromJSON(toJSON(marketData))
+    marketData <- data.frame(marketData)
+    
+    keepCols <- c("Contracts.TickerSymbol",
+                  "Contracts.LastTradePrice",
+                  "TimeStamp")
+    
+    marketData <- marketData[keepCols]
+    
+    marketData$Contracts.TickerSymbol <- unlist(marketData$
+                                                  Contracts.TickerSymbol)
+    marketData$Contracts.LastTradePrice <- unlist(marketData$
+                                                    Contracts.LastTradePrice)
+    marketData$TimeStamp <- gsub("T"," ", marketData$TimeStamp)
+    
+    marketData$TimeStamp <- as.POSIXlt(marketData$TimeStamp)
+    
+    marketData
+  }
+  # initialize the table 
+  serverData <<- GetData("VAGOV17")
+  # function to update server data
+  UpdateData <- function(){
+    serverData <<- rbind(GetData("VAGOV17"), serverData)
+  }
+  
+  
+  output$serverData <- DT::renderDataTable({
+    invalidateLater(60000, session) # 60 seconds
+    UpdateData()
+    serverData
+    })
 
-shinyServer(function(input, output) {
-  
-  series$DATE <- as.Date(series$DATE)
-  series$DGS10 <- as.numeric(series$DGS10)
-  
-  timeseries <- ts(series$DGS10, 
-                   start = c(2012,2,16), 
-                   frequency = 365.25)
-  
-  
-  
-  output$distPlot <- renderPlot({
-    model <- forecast(Arima(timeseries, order = c(input$AR,
-                                                  input$DF,
-                                                  input$MA)))
-    plot(model)
-    })
-  
-  output$modelData <- renderPrint({
-    model <- Arima(timeseries, order = c(input$AR,
-                                         input$DF,
-                                         input$MA))
-    model$coef
-    })
 })
--- ui.R
+++ ui.R
@@ -1,44 +1,14 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 
 shinyUI(fluidPage(
 
   # Application title
-  titlePanel("Time series with forecast package"),
+  titlePanel("Streaming PredictIt Data"),
 
   # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      numericInput(inputId="AR",
-                   label="Auto Regressive terms",
-                   min = 0,
-                   max = 3,
-                   value = 1),
-      numericInput(inputId="MA",
-                   label="Moving Average terms",
-                   min = 0,
-                   max = 3,
-                   value = 1),
-      numericInput(inputId="DF",
-                   label="Differencing",
-                   min = 0, 
-                   max = 3,
-                   value = 0),
-      verbatimTextOutput("modelData")
-    
-    ),
-    
-    
-
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
+  mainPanel(
+     DT::dataTableOutput("serverData")
     )
   )
-))
+)
+
